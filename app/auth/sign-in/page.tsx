de0b8ec5e69bdb2c49892e24a4d20f87a21a95ce
Emmanuel Quarcoo <emmanuelq150@gmail.com>
2025-11-05 11:42:51 +0100

    site url

--- app/auth/sign-in/page.tsx
+++ app/auth/sign-in/page.tsx
@@ -12,13 +12,18 @@ export default async function SignInPage() {
     return redirect("/");
   }
 
+  const getSiteUrl =
+    `https://${process.env.VERCEL_URL}` ||
+    process.env.NEXT_PUBLIC_SITE_URL ||
+    "http://localhost:3000";
+
   async function signInWithGoogle() {
     "use server";
     const supabase = await createClient();
     const { data, error } = await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: `${process.env.NEXT_PUBLIC_SITE_URL}/auth/callback`,
+        redirectTo: `${getSiteUrl}/auth/callback`,
       },
     });
 
